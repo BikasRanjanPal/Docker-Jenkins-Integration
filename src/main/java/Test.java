23d04b4b0db947f415c40e2bbe3288d7e1894c34
BikasRanjanPal <49264261+BikasRanjanPal@users.noreply.github.com>
2020-03-17 12:47:41 +0530

    Update Test.java

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,5 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("Docker-Jenkins-Integration-Successful");
+        System.out.println("Docker-Jenkins-Integration-Successfull");
     }
 }
